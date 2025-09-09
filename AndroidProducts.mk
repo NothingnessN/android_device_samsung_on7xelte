1339ef60b3ff1d41b6604dafa2069ada6613fd96
NothingnessN <oyuncucaliskan4555@gmail.com>
2025-09-09 19:42:49 +0000

    Adapt device tree for DotOS (Android 11)

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_on7xelte.mk
+    $(LOCAL_DIR)/dot_on7xelte.mk
 
 COMMON_LUNCH_CHOICES += \
-    lineage_on7xelte-user \
-    lineage_on7xelte-userdebug \
-    lineage_on7xelte-eng
+    dot_on7xelte-user \
+    dot_on7xelte-userdebug \
+    dot_on7xelte-eng
--- dot_on7xelte.mk
+++ dot_on7xelte.mk
@@ -21,10 +21,10 @@ $(call inherit-product, device/samsung/on7xelte/device.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/dot/config/common.mk)
 
 # Device identifier
-PRODUCT_NAME := lineage_on7xelte
+PRODUCT_NAME := dot_on7xelte
 PRODUCT_DEVICE := on7xelte
 PRODUCT_BRAND := Samsung
 PRODUCT_MODEL := Samsung Galaxy J7 Prime
