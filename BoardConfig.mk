b1bf68ed5339be25b99ce8c85e9ac792aa16ac8b
Anil <yogurtbirsah65@gmail.com>
2026-01-19 10:22:48 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -44,12 +44,6 @@ TARGET_CPU_CORTEX_A53 := true
 # Binder
 TARGET_USES_64_BIT_BINDER := true
 
-# VNDK VERSION
-BOARD_VNDK_VERSION := current
-
-# Dedupe VNDK libraries with identical core variants.
-TARGET_VNDK_USE_CORE_VARIANT := true
-
 # Broken Rules
 BUILD_BROKEN_DUP_RULES := true
 
